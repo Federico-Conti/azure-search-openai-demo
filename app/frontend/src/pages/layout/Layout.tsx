35ec3a7997e5c15524e19029f88ca789ec9d05e6
federico conti <federicoconti1702@gmail.com>
2024-06-07 11:34:48 +0000

    Creat new orphan from DEV:  - Sync forked repo until  Commits on Jun 5, 2024

---
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,56 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+import { useLogin } from "../../authConfig";
+
+import { LoginButton } from "../../components/LoginButton";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>ChatICT V3.0</h3>
+                        <p className={styles.headersubTitle}>AI-generated answer</p>
+                    </Link>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Chat (preview)
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    About
+                                </NavLink>
+                            </li>
+                            {/* <li className={styles.headerNavLeftMargin}>
+                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
+                                    <img
+                                        src={github}
+                                        alt="Github logo"
+                                        aria-label="Link to github repository"
+                                        width="20px"
+                                        height="20px"
+                                        className={styles.githubLogo}
+                                    />
+                                </a>
+                            </li> */}
+                        </ul>
+                    </nav>
+                    {/* <h4 className={styles.headerRightText}>AI-generated answer</h4> */}
+                    {useLogin && <LoginButton />}
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
